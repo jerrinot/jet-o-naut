7a0774fc11fb8bba49adf53bfeb9c12d39314ffa
Jaromir Hamala <jaromir.hamala@gmail.com>
2019-01-17 13:23:59 +0300

    configured app properties

--- src/main/java/info/jerrinot/micronautjet/ParametrizedFilter.java
+++ src/main/java/info/jerrinot/micronautjet/ParametrizedFilter.java
@@ -1,10 +1,16 @@
 package info.jerrinot.micronautjet;
 
+import io.micronaut.context.annotation.Property;
+
 import javax.inject.Singleton;
 
 @Singleton
 public class ParametrizedFilter {
+
+    @Property(name = "application.threshold")
+    private long threshold;
+
     public boolean test(long value) {
-        return (value > 0);
+        return (value > threshold);
     }
 }
--- src/main/java/info/jerrinot/micronautjet/SourceContext.java
+++ src/main/java/info/jerrinot/micronautjet/SourceContext.java
@@ -1,19 +1,29 @@
 package info.jerrinot.micronautjet;
 
+import io.micronaut.context.annotation.Property;
+
+import javax.annotation.PostConstruct;
 import javax.inject.Singleton;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 @Singleton
 public final class SourceContext {
-    private static final long INTERVAL_NANOS = TimeUnit.MILLISECONDS.toNanos(500);
+    @Property(name = "application.interval-millis")
+    private long intervalMillis;
 
+    private long intervalNanos;
     private final Random r = new Random();
     private long lastTimestamp;
 
+    @PostConstruct
+    public void postConstruct() {
+        intervalNanos = TimeUnit.MILLISECONDS.toNanos(intervalMillis);
+    }
+
     public Long nextOrNull() {
         long now = System.nanoTime();
-        if (lastTimestamp + INTERVAL_NANOS < now) {
+        if (lastTimestamp + intervalNanos < now) {
             lastTimestamp = now;
             return r.nextLong();
         }
